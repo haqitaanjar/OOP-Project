82c503aa4e269b96ba8d33f43befa0916dc4efc3
haqitaanjar <haqitaanjar17@gmail.com>
2017-05-03 13:14:59 +0700

    Add files via upload

---
+++ Cherry.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tubes;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Cherry extends GameObject{
+    private int timeSummon;
+    
+    public void setSummon(int summonPlaceX,int summonPlaceY){
+        Cherry C = new Cherry();
+        C.summonPlaceX = 0;
+        C.summonPlaceY = 0;
+    }
+    
+}
